85c36148e2e6a6bd9d6f737205585b1f74f56949
Mon Blan <mon@blanc.com>
2021-02-09 12:35:55 +0300

    Rename x,y to row,col

--- gol.py
+++ gol.py
@@ -75,23 +75,23 @@ def update_cell_status(cell, state):
     state_width  = get_state_width(state)
     state_height = get_state_height(state)
 
-    cell_x      = cell.get('x')
-    cell_y      = cell.get('y')
-    cell_status = cell.get('status')
+    cell_row      = cell.get('row')
+    cell_col      = cell.get('col')
+    cell_status   = cell.get('status')
 
-    logging.debug(f"cell coords: {cell_x},{cell_y} | status: {cell_status}")
+    logging.debug(f"cell coords: {cell_row},{cell_col} | status: {cell_status}")
 
     live_neighbors = 0
 
-    for neighbor_x in range(cell_x - 1, cell_x + 2):
-        if neighbor_x >= 0 and neighbor_x < state_width:
-            for neighbor_y in range(cell_y - 1, cell_y + 2):
-                if neighbor_y >= 0 and neighbor_y < state_height:
-                    if neighbor_x == cell_x and neighbor_y == cell_y:
+    for neighbor_row in range(cell_row - 1, cell_row + 2):
+        if neighbor_row >= 0 and neighbor_row < state_width:
+            for neighbor_col in range(cell_col - 1, cell_col + 2):
+                if neighbor_col >= 0 and neighbor_col < state_height:
+                    if neighbor_row == cell_row and neighbor_col == cell_col:
                         continue
                     else:
-                        logging.debug(f"neighbor coords: {neighbor_x},{neighbor_y} | status: {state[neighbor_x][neighbor_y]}")
-                        if state[neighbor_x][neighbor_y] == 1:
+                        logging.debug(f"neighbor coords: {neighbor_row},{neighbor_col} | status: {state[neighbor_row][neighbor_col]}")
+                        if state[neighbor_row][neighbor_col] == 1:
                             live_neighbors += 1
 
     if cell_status == 1:
@@ -127,10 +127,10 @@ def next_state(state):
 
     next_state = dead_state(state_width, state_height)
 
-    for x in range(0, state_width):
-        for y in range(0, state_height):
-            cell = {'x':x, 'y':y, 'status':state[x][y]}
-            next_state[x][y] = update_cell_status(cell, state)
+    for row in range(0, state_width):
+        for col in range(0, state_height):
+            cell = {'row':row, 'col':col, 'status':state[row][col]}
+            next_state[row][col] = update_cell_status(cell, state)
 
     return next_state
 
